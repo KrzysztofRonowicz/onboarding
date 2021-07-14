e105b2bff1be2eac1ab7e200ab30117523400112
kron <ronowicz12345@gmail.com>
2021-07-14 11:08:15 +0200

    Update: Robię teraz tutorial tour of heroes punkt 4

--- Tour-of-Heroes/src/app/heroes/heroes.component.ts
+++ Tour-of-Heroes/src/app/heroes/heroes.component.ts
@@ -13,6 +13,9 @@ export class HeroesComponent implements OnInit {
   hero = 'Windstorm';
   */
 
+  heroes = HEROES;
+  selectedHero?: Hero;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
@@ -23,4 +26,7 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
