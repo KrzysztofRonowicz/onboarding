c61d2e45c4a329f78c863c9bbadd745206cff58d
kron <ronowicz12345@gmail.com>
2021-07-19 15:00:10 +0200

    Update: Zapoznanie z Angular Material

--- Tour-of-Heroes/src/app/app.module.ts
+++ Tour-of-Heroes/src/app/app.module.ts
@@ -17,6 +17,11 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter.reducer';
 import { MyCounterComponent } from "./my-counter/my-counter.component";
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from "@angular/material/button";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -36,7 +41,12 @@ import { MyCounterComponent } from "./my-counter/my-counter.component";
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, {dataEncapsulation: false}
     ),
-    StoreModule.forRoot({ count: counterReducer }),
+    StoreModule.forRoot({count: counterReducer}),
+    MatSliderModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
